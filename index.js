812aa932806af65da9657b3044986fbfe5db8b49
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-02-26 10:47:38 +0530

    added node file

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 /* eslint-disable no-console */
-console.log(__dirname);
-console.log(__filename);
+const http = require('http');
+
+const port = process.env.PORT || 1337;
+
+// now the server responds with an API
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ text: 'hi', primes: [2, 3, 5, 7] }));
+});
+
+server.listen(port);
+console.log(`Server listening on port ${port}`);
---
+++ people.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+const people = ['suryaraj', 'srijeeta', 'alpha'];
+const a = 6;
+function test() {
+    console.log('test');
+}
+module.exports = {
+    people,
+    a,
+    test,
+};
