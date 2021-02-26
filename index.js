c7e1cd299921e281f34fc06de4ec102b89a3a3b7
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-02-26 11:22:13 +0530

    learning basic routing and dynamic response

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 /* eslint-disable no-console */
 const http = require('http');
+const querystring = require('querystring');
+// both http and querystring are core modules
+// core = pre installed in node.js
 
 const port = process.env.PORT || 1337;
 
-// now the server responds with an API
-const server = http.createServer((req, res) => {
+// basic routing
+const respondText = (req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello');
+};
+
+const respondJson = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ text: 'hi', primes: [2, 3, 5, 7] }));
+    res.end(JSON.stringify({ text: 'hello JSON', nums: [2, 3, 5, 7, 11] }));
+};
+
+const respondNotFound = (req, res) => {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+};
+
+const respondEcho = (req, res) => {
+    const { input = '' } = querystring.parse(req.url.split('?').slice(1).join(''));
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+        normal: input,
+        shouty: input.toUpperCase();
+        characterCount: input.length,
+        backwards: input.split(' ').reverse().join('')
+    }))
+};
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') return respondText(req, res);
+    if (req.url === '/json') return respondJson(req, res);
+    if (req.url.match(/^\/echo/)) return respondEcho(req, res);
+    respondNotFound(req, res);
 });
 
 server.listen(port);
